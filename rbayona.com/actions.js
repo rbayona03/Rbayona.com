ac3a843c89277e8ba4dc846f6e39fc3642261c0f
Robert bayona <69874938+rbayona03@users.noreply.github.com>
2021-03-06 16:59:38 -0500

    Add files via upload
    added to top button and changed some layout features on main page

--- rbayona.com/actions.js
+++ rbayona.com/actions.js
@@ -6,5 +6,20 @@ function resFunct(){
 
 };
 
+topButton = document.getElementById("topBtn");
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
