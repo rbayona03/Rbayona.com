e584185f1c3548122d05eec99b0726f2ca921ad4
Robert bayona <69874938+rbayona03@users.noreply.github.com>
2021-03-09 16:03:17 -0500

    Add files via upload
    Added Mobile responsive and Hamburger menu.

--- rbayona.com/actions.js
+++ rbayona.com/actions.js
@@ -16,10 +16,50 @@ function scrollFunction() {
   } else {
     topButton.style.display = "none";
   }
-}
+};
 
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
+
+let toggleNavStaus = false;
+
+let toggleNav = function() {
+  let getSidebar = document.querySelector(".sideBar");
+  let getSidebarUl = document.querySelector(".asideLinks");
+  let getSideLinks = document.querySelectorAll(".asideLinks a");
+
+  if (toggleNavStaus === false) {
+    getSidebarUl.style.visibility = "visible";
+    getSidebar.style.width = "50%";
+    getSidebarUl.style.opacity = "1";
+
+    let arrayLength = getSideLinks.length;
+    for(let i = 0; i < arrayLength; i++){
+      getSideLinks[i].style.opacity = "1";
+    }
+
+    toggleNavStaus = true;
+
+  }
+
+
+  else if (toggleNavStaus === true) {
+    getSidebar.style.width = "0%";
+    getSidebarUl.style.opacity = "0";
+
+    let arrayLength = getSideLinks.length;
+    for(let i = 0; i < arrayLength; i++){
+      getSideLinks[i].style.opacity = "0";
+    }
+
+    getSidebarUl.style.visibility = "hidden";
+
+    toggleNavStaus = false;
+  }
+
+
+
+};
 
